5af33921f93b8f2692c54a1cf8019ea6f3404a20
Jhon <jhonjairo0897@hotmail.com>
2025-07-07 18:45:13 -0400

    Leetcode 215 Medium - Kth Largest Element in Array

---
+++ Medium/215 Kth Largest Element in Array.js
@@ -0,0 +1,45 @@
+/*
+215. Kth Largest Element in an Array
+Medium
+Topics
+premium lock icon
+Companies
+Given an integer array nums and an integer k, return the kth largest element in the array.
+
+Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Can you solve it without sorting?
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,1,5,6,4], k = 2
+Output: 5
+Example 2:
+
+Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+Output: 4
+ 
+
+Constraints:
+
+1 <= k <= nums.length <= 105
+-104 <= nums[i] <= 104
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function(nums, k) {
+    nums.sort((a,b)=> a-b);
+    console.log(nums);
+    return nums[nums.length - k];
+};
+
+let nums = [3,2,3,1,2,4,5,5,6,7,7,8,2,3,1,1,1,10,11,5,6,2,4,7,8,5,6];
+let k = 2;
+
+console.log(findKthLargest(nums, k)); //10
